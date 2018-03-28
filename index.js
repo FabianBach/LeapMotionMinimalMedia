7c571e53d9c011045a5f3379e6aa96dc2119bc61
Bach, Fabian <fabian.bach@arsmedium.com>
2018-03-28 18:21:16 +0200

    For great justice.

--- index.js
+++ index.js
@@ -4,4 +4,29 @@ var leapjs = require('leapjs');
 var leapController = new leapjs.Controller();
 
 
-// TODO: everything
+leapController.on('deviceFrame', function(frame) {
+  checkGestures(frame.gestures);
+});
+leapController.connect();
+
+
+function checkGestures(gestures) {
+  // loop through available gestures
+  for(var i = 0; i < gestures.length; i++){
+    var gesture = gestures[i];
+    var type = gesture.type;
+
+    switch(type){
+      case "keyTap":
+        if (gesture.state == "stop") {
+          console.log('keyTap');
+          mediaController.executeCommand('pause', function(err, result) {
+            if(!err) {
+              console.log('PAUSE');
+            }
+          });
+        }
+        break;
+    }
+  }
+}
